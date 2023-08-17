d8c90f805fa8403c2d751e83ed47b00ace4499ac
crocsg <pub.sg@free.fr>
2023-08-17 15:36:34 +0200

    thermistor config

--- Firmware/configuration/mega2560/Configuration.h
+++ Firmware/configuration/mega2560/Configuration.h
@@ -531,7 +531,7 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  *
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 998
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
