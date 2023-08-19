f9ba44f630fc44d0706555f6e98943896a0019bc
crocsg <pub.sg@free.fr>
2023-08-19 08:58:01 +0200

    BrailleRAP electromagnet config

--- Firmware/configuration/mega2560/Configuration_adv.h
+++ Firmware/configuration/mega2560/Configuration_adv.h
@@ -3398,7 +3398,7 @@
 #define SPINDLE_FEATURE
 //#define LASER_FEATURE
 #if EITHER(SPINDLE_FEATURE, LASER_FEATURE)
-  #define SPINDLE_LASER_ACTIVE_STATE    HIGH    // Set to "HIGH" if SPINDLE_LASER_ENA_PIN is active HIGH
+  #define SPINDLE_LASER_ACTIVE_STATE    LOW    // Set to "HIGH" if SPINDLE_LASER_ENA_PIN is active HIGH
 
   #define SPINDLE_LASER_USE_PWM                // Enable if your controller supports setting the speed/power
   #if ENABLED(SPINDLE_LASER_USE_PWM)
