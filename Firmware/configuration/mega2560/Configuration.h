11b761637e4dfe2fc560053c7bfd99aae154e19b
crocsg <pub.sg@free.fr>
2023-08-28 20:53:27 +0200

    enable extaruder to avoid errors

--- Firmware/configuration/mega2560/Configuration.h
+++ Firmware/configuration/mega2560/Configuration.h
@@ -172,7 +172,7 @@
 //#define U_DRIVER_TYPE  A4988
 //#define V_DRIVER_TYPE  A4988
 //#define W_DRIVER_TYPE  A4988
-//#define E0_DRIVER_TYPE DRV8825
+#define E0_DRIVER_TYPE DRV8825
 //#define E1_DRIVER_TYPE A4988
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
@@ -227,7 +227,7 @@
 
 // This defines the number of extruders
 // :[0, 1, 2, 3, 4, 5, 6, 7, 8]
-#define EXTRUDERS 0
+#define EXTRUDERS 1
 
 // Generally expected filament diameter (1.75, 2.85, 3.0, ...). Used for Volumetric, Filament Width Sensor, etc.
 #define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
@@ -1162,16 +1162,16 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80*2, 87, 45, 45 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80*2, 87, 45}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80*2, 87, 45, 45 }
+
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-//#define DEFAULT_MAX_FEEDRATE          { 300, 300, 300, 25 }
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 300}
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 300, 25 }
+
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1184,8 +1184,8 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-//#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 100, 10000 }
-#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 100}
+#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 1500, 1500 }
+
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
