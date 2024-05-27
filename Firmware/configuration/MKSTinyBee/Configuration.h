3b05ea69b5d6768458d74f806a63d522143d0b4e
crocsg <pub.sg@free.fr>
2024-05-27 10:02:02 +0200

    enable options for paper sensing

--- Firmware/configuration/MKSTinyBee/Configuration.h
+++ Firmware/configuration/MKSTinyBee/Configuration.h
@@ -1096,7 +1096,7 @@
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 #define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define I_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define J_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
@@ -1633,7 +1633,7 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
+#define INVERT_X_DIR true
 #define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
@@ -1687,8 +1687,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 420
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -2087,7 +2087,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (20*60), (20*60), (4*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
@@ -3344,7 +3344,7 @@
 
 #define BRAILLERAP_AUTODISABL_MAGNET	1
 #define PAPER_LOADING_HOME_Y  1
-#define PAPER_LOADING_MAX_LOAD_TRAVEL 300
+#define PAPER_LOADING_MAX_LOAD_TRAVEL 430
 #define PAPER_LOADING_LOAD_TRAVEL 25
 
 #if BRAILLERAP_DISABLE_SOLENOID
